8e04309fdfa4b82af08bec73127274a469e82660
SondreB <sondre@outlook.com>
2022-11-24 17:53:10 +0100

    Add support for syncing whole history
    WIP: Need to keep state so it doesn't run full sync each time

--- src/sync.ts
+++ src/sync.ts
@@ -8,6 +8,68 @@ export class SyncProcess {
 		console.log('DID Servers:', this.servers);
 	}
 
+	async process(sequence: number, server: string) {
+		const url = `${server}/1.0/log/${sequence}`;
+		console.log('Fetching:', url);
+
+		// Used to populate local instance of Blockcore DID Server:
+		const rawResponse = await fetch(url, {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		});
+
+		const content = await rawResponse.json();
+
+		console.log(content.length);
+
+		if (content.length == 0) {
+			console.log('No more data to process, sync completed.');
+			return;
+		}
+
+		for (let i = 0; i < content.length; i++) {
+			const did = content[i].did;
+			const version = content[i].ver;
+
+			console.log('Do we have DID: ', did);
+			console.log('Version: ', version);
+
+			const doc = await this.server.resolve(did, version);
+
+			if (didNotFound(doc)) {
+				const fetchUrl = `${server}/1.0/identifiers/${did}?versionId=${version}`;
+
+				console.log('Fetch URL: ', fetchUrl);
+				// TODO: The LATEST didDocument is not accessible with the version parameter... what to do?
+				const rawResponse2 = await fetch(fetchUrl, {
+					method: 'GET',
+					headers: {
+						Accept: 'application/json',
+						'Content-Type': 'application/json',
+					},
+				});
+
+				const content = await rawResponse2.json();
+
+				if (content) {
+					const jws = content.didDocumentMetadata.proof.jwt;
+					await this.server.request(jws);
+				}
+			}
+
+			// Keep track of the last sequence:
+			sequence = content[i].seq;
+		}
+
+		console.log('Last sequence: ', sequence);
+
+		// Continue processing until we're done.
+		await this.process(sequence, server);
+	}
+
 	async run() {
 		console.log(`${new Date()}: Running Sync Process...`);
 
@@ -19,60 +81,14 @@ export class SyncProcess {
 		for (let i = 0; i < this.servers.length; i++) {
 			const server = this.servers[i];
 
-			// Load last sequence for this server:
-			let sequence = 0;
-
-			console.log(`Sync with: ${server}`);
-			console.log('Fetching:', `${server}/1.0/log/0`);
-
-			// Used to populate local instance of Blockcore DID Server:
-			const rawResponse = await fetch(`${server}/1.0/log/0`, {
-				method: 'GET',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json',
-				},
-			});
-
-			const content = await rawResponse.json();
-
-			console.log(content.length);
-
-			for (let i = 0; i < content.length; i++) {
-				const did = content[i].did;
-				const version = content[i].ver;
-
-				console.log('Do we have DID: ', did);
-				console.log('Version: ', version);
-
-				const doc = await this.server.resolve(did, version);
-
-				if (didNotFound(doc)) {
-					const fetchUrl = `${server}/1.0/identifiers/${did}?versionId=${version}`;
-
-					console.log('Fetch URL: ', fetchUrl);
-					// TODO: The LATEST didDocument is not accessible with the version parameter... what to do?
-					const rawResponse2 = await fetch(fetchUrl, {
-						method: 'GET',
-						headers: {
-							Accept: 'application/json',
-							'Content-Type': 'application/json',
-						},
-					});
-
-					const content = await rawResponse2.json();
-
-					if (content) {
-						const jws = content.didDocumentMetadata.proof.jwt;
-						await this.server.request(jws);
-					}
-				}
-
-				// Keep track of the last sequence:
-				sequence = content[i].seq;
+			if (!server) {
+				continue;
 			}
 
-			console.log('Last sequence: ', sequence);
+			// Load last sequence for this server:
+			const sequence = 0;
+
+			await this.process(sequence, server);
 		}
 	}
 }
